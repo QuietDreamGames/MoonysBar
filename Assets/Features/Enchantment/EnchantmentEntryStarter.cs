1de521f28cc508d4c8f92af3741d9120be3740f6
maxim-nichiforov-5ql <maxim.nichiforov@5qlogistics.com>
2025-05-20 16:18:19 +0300

    used line renderer to be able to draw enchantment pattern that will foreshadow player's task target.

--- Assets/Features/Enchantment/EnchantmentEntryStarter.cs
+++ Assets/Features/Enchantment/EnchantmentEntryStarter.cs
@@ -21,35 +21,25 @@ namespace Features.Enchantment
                 return;
             }
 
-            lineRendererObject.startWidth = 0.5f;
-            lineRendererObject.endWidth   = 0.5f;
+            lineRendererObject.startWidth = 1f;
+            lineRendererObject.endWidth   = 1f;
+
+            lineRendererObject.numCornerVertices = 50;
+            lineRendererObject.numCapVertices    = 50;
 
             lineRendererObject.positionCount = layout.Connections.Count;
 
             var path = FindLinePath(layout);
 
             for (var i = 0; i < path.Count; i++)
-            {
                 lineRendererObject.SetPosition(i,
-                    _playingFieldService.ConvertRelativeToWorldPosition(layout.GetNodeByIndex(path[i]).InitialPosition));
-            }
-
-            // for (var i = 1; i < layout.Connections.Count; i++)
-            // {
-            //     var connection = layout.Connections[i];
-            //     var startNode  = layout.Nodes[connection.NodeA.Index];
-            //     var endNode    = layout.Nodes[connection.NodeB.Index];
-            //
-            //     lineRendererObject.SetPosition(i * 2,
-            //         _playingFieldService.ConvertRelativeToWorldPosition(startNode.InitialPosition));
-            //     lineRendererObject.SetPosition(i * 2 + 1,
-            //         _playingFieldService.ConvertRelativeToWorldPosition(endNode.InitialPosition));
-            // }
+                    _playingFieldService.ConvertRelativeToWorldPosition(layout.GetNodeByIndex(path[i])
+                        .InitialPosition));
         }
 
-        private List<Vector2> FindLinePath(EnchantmentGraphData graphData)
+        private static List<int> FindLinePath(EnchantmentGraphData graphData)
         {
-            var edges = new List<GraphPathfinder.PathEdge>();
+            var edges = new List<(int, int)>();
 
             for (var i = 0; i < graphData.Connections.Count; i++)
             {
@@ -57,65 +47,23 @@ namespace Features.Enchantment
                 var startNode  = graphData.Nodes[connection.NodeA.Index];
                 var endNode    = graphData.Nodes[connection.NodeB.Index];
 
-                edges.Add(new GraphPathfinder.PathEdge(startNode.InitialPosition, endNode.InitialPosition));
+                edges.Add((startNode.Index, endNode.Index));
             }
 
-            graphData.Nodes.Sort((a, b) => a.Index.CompareTo(b.Index));
-            var firstNode = graphData.Nodes[0];
+            // let's suppose edges are built in the way they form the path we need.
+            // then we only need to use the first edge's start node as the start node,
+            // and all the next edges finish nodes as the path
 
-            return GraphPathfinder.FindLongestPath(firstNode.Index, edges);
-        }
-    }
-
-    public class GraphPathfinder
-    {
-        public static List<Vector2> FindLongestPath(Vector2 start, List<PathEdge> edges)
-        {
-            var longestPath = new List<Vector2>();
-
-            void DFS(Vector2 current, List<Vector2> path, HashSet<PathEdge> visited)
+            var path = new List<int>();
+            path.Add(graphData.Connections[0].NodeA.Index);
+            for (var i = 0; i < edges.Count; i++)
             {
-                path.Add(current);
-                if (path.Count > longestPath.Count)
-                    longestPath = new List<Vector2>(path);
-
-                foreach (var edge in edges)
-                    if (!visited.Contains(edge) && (edge.A == current || edge.B == current))
-                    {
-                        var next = edge.A == current ? edge.B : edge.A;
-                        visited.Add(edge);
-                        DFS(next, path, visited);
-                        visited.Remove(edge);
-                    }
-
-                path.RemoveAt(path.Count - 1);
+                var connection = edges[i];
+                path.Add(connection.Item2);
             }
 
-            DFS(start, new List<Vector2>(), new HashSet<PathEdge>());
-
-            return longestPath;
-        }
-
-        public class PathEdge
-        {
-            public Vector2 A, B;
-
-            public PathEdge(Vector2 a, Vector2 b)
-            {
-                A = a;
-                B = b;
-            }
-
-            // Undirected edge equality
-            public bool Equals(PathEdge other)
-            {
-                return (A == other.A && B == other.B) || (A == other.B && B == other.A);
-            }
-
-            public override int GetHashCode()
-            {
-                return A.GetHashCode() ^ B.GetHashCode();
-            }
+            return path;
+            // return GraphHelper.FindLongestPath(firstNode.Index, edges);
         }
     }
 }
