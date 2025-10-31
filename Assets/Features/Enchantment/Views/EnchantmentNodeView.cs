fabd38abbff6561db43b2cfb6620585bda70c8e6
odinlevi <odinlevi@gmail.com>
2025-10-31 16:43:46 +0200

    working on enchantment view model. looking for refactoring view to presentation and viewmodel to view. (wip)

--- Assets/Features/Enchantment/Views/EnchantmentNodeView.cs
+++ Assets/Features/Enchantment/Views/EnchantmentNodeView.cs
@@ -1,12 +1,28 @@
+using System;
 using Features.View;
 
 namespace Features.Enchantment.Views
 {
     public class EnchantmentNodeView : TweenedView
     {
-        public virtual void Initialize(object nodeViewModel)
+        public  void Initialize(object nodeViewModel)
         {
             base.Initialize();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            base.OnUpdate(deltaTime);
+        }
+
+        public event Action OnReturnToPool;
+
+        public void ReturnToPool()
+        {
+            OnReturnToPool?.Invoke();
+            OnReturnToPool = null;
+
+            ClearAnimations();
         }
     }
 }
