e080727388b35a10f9aaa496e4d9362341d91bcb
odinlevi <odinlevi@gmail.com>
2025-11-13 15:12:57 +0200

    working enchantment through (wip)

--- Assets/Features/Enchantment/EnchantmentPathController.cs
+++ Assets/Features/Enchantment/EnchantmentPathController.cs
@@ -1,14 +1,17 @@
+using System;
 using System.Collections.Generic;
 using Features.CameraSystem;
 using Features.Enchantment.Datas;
+using Features.Enchantment.Enums;
 using Features.Enchantment.Presenters;
 using Features.InputDispatching;
+using Features.TimeSystem.Interfaces.Handlers;
 using UnityEngine;
 using VContainer;
 
 namespace Features.Enchantment
 {
-    public class EnchantmentPathController
+    public class EnchantmentPathController : IUpdateHandler
     {
         [Inject] private readonly CameraHolderService                 _cameraHolderService;
         [Inject] private readonly EnchantmentElementsHolderAndUpdater _elementsHolder;
@@ -19,7 +22,6 @@ namespace Features.Enchantment
         private EnchantmentGraphData _layout;
 
         private Stack<TempNodeConnectionLine> _tempConnectionLines;
-
 
         public void SetLayout(EnchantmentGraphData layout)
         {
@@ -35,38 +37,65 @@ namespace Features.Enchantment
                 Debug.LogError("Enchantment Handle not found in Elements Holder.");
         }
 
-        public void OnPointerColliderHeld(EnchantmentPointerCollider nodeCollider, bool withHandle, bool isHeld)
+        public void OnPointerColliderHeld(EnchantmentPointerCollider heldCollider, bool isHeld)
+        {
+            if (_layout == null) return;
+
+            if (TryGetNodeIndexByCollider(collider: heldCollider, nodeIndex: out var nodeIndex))
+                // first node in path should just be added. the handle appears on this node and should follow the pointer.
+                // handle should collide with other nodes to create connections.
+                // from handle to pointer should be a temporary line.
+                // when handle collides with another node, if connection is legal, add to path and create permanent connection line. (for now still named temporary)
+                if (isHeld && _currentPath.Count == 0)
+                {
+                    _currentPath.Push(nodeIndex);
+                    if (_elementsHolder.TryGetEnchantmentHandle(out var handle))
+                    {
+                        handle.Item2.Show();
+                        handle.Item1.transform.position = _cameraHolderService.MainCamera.ScreenToWorldPoint(
+                            InputUtils.GetPrimaryPointerScreenPosition()
+                        );
+                        _tempConnectionLines.Push(new TempNodeConnectionLine(
+                            startPosition: handle.Item1.transform.position,
+                            endPosition: handle.Item1.transform.position
+                        ));
+
+                    }
+                }
+
+            if (heldCollider.PointerType == EnchantmentPointerColliderType.Handle)
+            {
+
+            }
+        }
+
+        public void OnHandleConnectsToNode(EnchantmentPointerCollider nodeCollider)
         {
             if (_layout == null) return;
 
             if (!TryGetNodeIndexByCollider(collider: nodeCollider, nodeIndex: out var nodeIndex))
                 return;
 
-            if (isHeld && _currentPath.Count == 0)
+            if (_currentPath.Count == 0)
+                return;
+
+            var fromNodeId = _currentPath.Peek();
+            var toNodeId   = nodeIndex;
+
+            if (IsConnectingLegal(fromNodeId: fromNodeId, toNodeId: toNodeId))
             {
+                // fixate connection
                 _currentPath.Push(nodeIndex);
-                if (_elementsHolder.TryGetEnchantmentHandle(out var handle))
-                {
-                    handle.Item2.Show();
-                    handle.Item1.transform.position = _cameraHolderService.MainCamera.ScreenToWorldPoint(
-                        InputUtils.GetPrimaryPointerScreenPosition()
-                    );
-                    _tempConnectionLines.Push(new TempNodeConnectionLine(
-                        startPosition: handle.Item1.transform.position,
-                        endPosition:   handle.Item1.transform.position
-                    ));
-                }
+                var currentTempLine = _tempConnectionLines.Peek();
+                currentTempLine.UpdateEndPosition(
+                    newEndPosition: _elementsHolder.TryFindEnchantmentNodeByIndex(index: nodeIndex,
+                        result: out var node)
+                        ? node.Item2.transform.position
+                        : Vector3.zero
+                );
             }
 
-            if (isHeld && _currentPath.Count > 2 && nodeIndex == _currentPath.Peek())
-            {
-                _
-
-            }
-        }
-
-        public void OnPointerColliderPassThrough()
-        {
+            // create new temp line from handle to pointer
         }
 
         private bool IsConnectingLegal(int fromNodeId, int toNodeId)
@@ -88,6 +117,8 @@ namespace Features.Enchantment
         private bool TryGetNodeIndexByCollider(EnchantmentPointerCollider collider, out int nodeIndex)
         {
             nodeIndex = -1;
+            if (collider.PointerType != EnchantmentPointerColliderType.Node) return false;
+
             var nodePresenter = collider.GetComponentInParent<EnchantmentNodePresenter>();
             if (nodePresenter == null)
             {
@@ -104,17 +135,25 @@ namespace Features.Enchantment
             Debug.LogError("Node Data not found for the given Node Presenter.");
             return false;
         }
+
+        public void OnUpdate(float deltaTime)
+        {
+
+        }
     }
 
     internal class TempNodeConnectionLine
     {
         public Vector2 EndPosition;
         public Vector2 StartPosition;
+        public bool    IsConnected;
 
         public TempNodeConnectionLine(Vector2 startPosition, Vector2 endPosition)
         {
             StartPosition = startPosition;
             EndPosition   = endPosition;
+
+            IsConnected = false;
 
             DrawLine();
         }
