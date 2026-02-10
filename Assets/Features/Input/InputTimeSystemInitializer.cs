da9da0d0293892cfbe6efcba20765808001b4986
odi <odinlevi@gmail.com>
2026-02-10 11:16:13 +0200

    fixes after a code review.

--- Assets/Features/Input/InputTimeSystemInitializer.cs
+++ Assets/Features/Input/InputTimeSystemInitializer.cs
@@ -6,12 +6,10 @@ namespace Features.Input
 {
     public class InputTimeSystemInitializer : IStartable
     {
-        [Inject] private readonly ITimeSystem     _timeSystem;
-        [Inject] private readonly IUpdateProvider _updateProvider;
+        [Inject] private readonly ITimeSystem _timeSystem;
 
         public void Start()
         {
-            _timeSystem.SetUpdateProvider(_updateProvider);
             _timeSystem.Initialize();
         }
     }
